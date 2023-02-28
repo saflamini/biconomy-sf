1c7e089579df6ffeb40f980d25173443f47e17eb
Sam Flamini <saflamini@gmail.com>
2023-02-27 19:49:12 -0600

    rm file

--- scripts/metaTx.js
+++ scripts/metaTx.js
@@ -3,19 +3,11 @@ const { Framework } = require("@superfluid-finance/sdk-core");
 const { ethers } = require("hardhat");
 const { Biconomy } = require("@biconomy/mexa");
 require("dotenv").config();
-const MoneyRouterABI = require("../artifacts/contracts/MoneyRouter.sol/MoneyRouter.json").abi;
 const sigUtil = require("eth-sig-util");
-const { AbiCoder } = require("ethers/lib/utils");
 const cfaABI = require("./cfaABI");
 const hostABI = require("./hostABI");
 
 
-//to run this script:
-//1) Make sure you've created your own .env file
-//2) Make sure that you have your network and accounts specified in hardhat.config.js
-//3) Make sure that you add the address of your own money router contract
-//4) Make sure that you change the params in the aclApproval operation to reflect the proper values
-//3) run: npx hardhat run scripts/aclApproval.js --network goerli
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -42,7 +34,7 @@ async function main() {
   let biconomy = new Biconomy(provider, {
     apiKey: 'ieykFwRbY.0b9399ed-52ec-4f34-acb0-7798ea9e30ec',
     debug: true,
-    contractAddresses: '0x22ff293e14F1EC3A09B137e9e06084AFd63adDF9',
+    contractAddresses: '0x22ff293e14f1ec3a09b137e9e06084afd63addf9',
   })
 
   biconomy.onEvent(biconomy.READY, async () => {
@@ -64,10 +56,10 @@ async function main() {
     getTransactionDescription(hostABI, callData).args;
 
 
-  const tx = cfaInterface.encodeFunctionData(
-    "deleteFlow",
-    [daix.address, mySigner.address, "0xaEFb2595E0681E16bBEB3FbA57dFA2CceA824A9A", "0x"]
-);  
+    const tx = cfaInterface.encodeFunctionData(
+      "authorizeFlowOperatorWithFullControl",
+      [daix.address, "0xaEFb2595E0681E16bBEB3FbA57dFA2CceA824A9A", "0x"]
+  );  
 
   // const operation = 
 
@@ -99,7 +91,7 @@ async function main() {
   
   
   let rawTx = {
-    to: '0x22ff293e14F1EC3A09B137e9e06084AFd63adDF9',
+    to: '0x22ff293e14f1ec3a09b137e9e06084afd63addf9',
     data: baseTx,
     gasLimit: 2000000,
     from: mySigner.address
@@ -112,21 +104,21 @@ async function main() {
   // console.log(forwardData)
 
   //note that the private key here cannot contain '0x'
-  let signature = sigUtil.signTypedMessage(new Buffer.from(`0x${process.env.PRIVATE_KEY}`, 'hex'), {data: forwardData.eip712Format }, 'V3')
+  // let signature = sigUtil.signTypedMessage(new Buffer.from(`0x${process.env.PRIVATE_KEY}`, 'hex'), {data: forwardData.eip712Format }, 'V3')
 
-  let data = {
-    signature: signature,
-    forwardRequest: forwardData.request,
-    rawTransaction: signedTx,
-    signatureType: biconomy.EIP712_SIGN
-  }
+  // let data = {
+  //   signature: signature,
+  //   forwardRequest: forwardData.request,
+  //   rawTransaction: signedTx,
+  //   signatureType: biconomy.EIP712_SIGN
+  // }
 
-  let p = biconomy.getEthersProvider()
-  let txHash = await p.send("eth_sendRawTransaction", [data]);
+  // let p = biconomy.getEthersProvider()
+  // let txHash = await p.send("eth_sendRawTransaction", [data]);
 
-  let receipt = await p.waitForTransaction(txHash);
+  // let receipt = await p.waitForTransaction(txHash);
 
-  console.log(receipt);
+  // console.log(receipt);
   })
 }
 
